a969ccb6c3eda156794ce0bbe9661cad5d5cb100
Volgoak <volgoak@gmail.com>
2017-04-11 15:19:23 +0300

    Activity saves fragment at destroy time.

--- app/src/main/java/xyz/volgoak/wordlearning/MainActivity.java
+++ app/src/main/java/xyz/volgoak/wordlearning/MainActivity.java
@@ -1,6 +1,7 @@
 package xyz.volgoak.wordlearning;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +13,9 @@ import android.view.MenuItem;
 import xyz.volgoak.wordlearning.training_utils.TrainingFabric;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, FragmentListener{
+
+    public static final String SAVED_FRAGMENT_TAG = "fragment_tag";
+    private Fragment mMyFragment;
 
     private ActionBarDrawerToggle drawerToggle;
     private DrawerLayout drawerLayout;
@@ -34,17 +38,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        if(savedInstanceState != null){
+            mMyFragment = getSupportFragmentManager().getFragment(savedInstanceState, SAVED_FRAGMENT_TAG);
+        }else mMyFragment = new StartFragment();
+
+        startFragment(mMyFragment);
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        StartFragment startFragment = new StartFragment();
+        /*StartFragment startFragment = new StartFragment();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, startFragment);
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+        ft.commit();*/
     }
 
     @Override
@@ -86,37 +96,36 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     public void startRedactorFragment(){
         RedactorFragment redactorFragment = new RedactorFragment();
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, redactorFragment);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+        startFragment(redactorFragment);
     }
 
     public void startTrainingWTFragment(){
         TrainingFragment trainingFragment = TrainingFragment.getWordTrainingFragment(TrainingFabric.WORD_TRANSLATION);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, trainingFragment);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+        startFragment(trainingFragment);
     }
 
     public void startTrainingTWFragment(){
         TrainingFragment trainingFragment = TrainingFragment.getWordTrainingFragment(TrainingFabric.TRANSLATION_WORD);
+        startFragment(trainingFragment);
+    }
+
+    public void startResultsFragment(int correctAnswers, int wrongAnswers){
+        ResultsFragment resultsFragment = ResultsFragment.getResultFragment(correctAnswers, wrongAnswers, this);
+        startFragment(resultsFragment);
+    }
+
+    public void startFragment(Fragment fragment){
+        mMyFragment = fragment;
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, trainingFragment);
+        ft.replace(R.id.fragment_container, fragment);
         ft.addToBackStack(null);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.commit();
     }
 
-    public void startResultsFragment(int correctAnswers, int wrongAnswers){
-        ResultsFragment resultsFragment = ResultsFragment.getResultFragment(correctAnswers, wrongAnswers, this);
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_container, resultsFragment);
-        ft.addToBackStack(null);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, SAVED_FRAGMENT_TAG, mMyFragment);
     }
 }
